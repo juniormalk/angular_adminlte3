da443c32a1a780e87b761346f0ac8e5d77239474
Demerval Alves Junior <juniormalk@gmail.com>
2020-04-08 15:55:51 -0300

    AdminLte3 Aplied

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
+import { HomeComponent } from './components/content/home/home.component';
+import { TenantsComponent } from './components/content/tenants/tenants.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ContentComponent } from './components/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+    component: ContentComponent,
+    children: [
+      {
+        path: 'tenants',
+        component: TenantsComponent
+      },
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ContentComponent } from './components/content/content.component';
+import { TenantsComponent } from './components/content/tenants/tenants.component';
+import { HomeComponent } from './components/content/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SettingsComponent,
+    MenuComponent,
+    ContentComponent,
+    TenantsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
